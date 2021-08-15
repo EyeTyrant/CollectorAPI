8971e8b006bcf94cc507d921e4f654b51f687909
EyeTyrant <1414jg@gmail.com>
2021-08-15 10:03:48 -0500

    only working with CORS set to "*", only working on FireFox browser

--- src/main/java/com/eyetyrantdesign/collector/controllers/CollectorController.java
+++ src/main/java/com/eyetyrantdesign/collector/controllers/CollectorController.java
@@ -14,7 +14,7 @@ import java.util.Collections;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200, https://eyetyrant.github.io/collectorui", allowCredentials = "true")
+@CrossOrigin(origins = "*", allowCredentials = "true")
 //@CrossOrigin(allowCredentials = "true")
 public class CollectorController {
 
