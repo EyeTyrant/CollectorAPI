5c25204409ed1b619863514f2142dbf23b7badd2
EyeTyrant <1414jg@gmail.com>
2021-08-15 09:54:15 -0500

    https://eyetyrant.github.io/collectorui

--- src/main/java/com/eyetyrantdesign/collector/controllers/AuthenticationController.java
+++ src/main/java/com/eyetyrantdesign/collector/controllers/AuthenticationController.java
@@ -16,7 +16,7 @@ import javax.validation.Valid;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin(origins = "*", allowCredentials = "true")
+@CrossOrigin(origins = "http://localhost:4200, https://eyetyrant.github.io", allowCredentials = "true")
 //@CrossOrigin(allowCredentials = "true")
 public class AuthenticationController {
 
