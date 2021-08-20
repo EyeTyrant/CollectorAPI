2a5cb98d57ec9c33e9a409db7f61fa2607b2f2c5
EyeTyrant <1414jg@gmail.com>
2021-08-20 17:15:49 -0500

    setHeader

--- src/main/java/com/eyetyrantdesign/collector/AuthenticationFilter.java
+++ src/main/java/com/eyetyrantdesign/collector/AuthenticationFilter.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseCookie
+import org.springframework.http.ResponseCookie;
 
 public class AuthenticationFilter extends HandlerInterceptorAdapter {
 
