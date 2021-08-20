a6cfdccd0df7a0673450d359d4a1e418c92155fd
EyeTyrant <1414jg@gmail.com>
2021-08-20 18:00:37 -0500

    setHeader

--- src/main/java/com/eyetyrantdesign/collector/controllers/AuthenticationController.java
+++ src/main/java/com/eyetyrantdesign/collector/controllers/AuthenticationController.java
@@ -16,7 +16,7 @@ import javax.validation.Valid;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*", allowCredentials = "true")
 //@CrossOrigin(allowCredentials = "true")
 public class AuthenticationController {
 
--- src/main/java/com/eyetyrantdesign/collector/controllers/CollectorController.java
+++ src/main/java/com/eyetyrantdesign/collector/controllers/CollectorController.java
@@ -14,7 +14,7 @@ import java.util.Collections;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*", allowCredentials = "true")
 //@CrossOrigin(allowCredentials = "true")
 public class CollectorController {
 
