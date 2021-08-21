fe3db47c80a119e9280b197cd2347511a46168ed
EyeTyrant <1414jg@gmail.com>
2021-08-21 14:18:41 -0500

    setHeader

--- src/main/java/com/eyetyrantdesign/collector/controllers/CollectorController.java
+++ src/main/java/com/eyetyrantdesign/collector/controllers/CollectorController.java
@@ -14,7 +14,7 @@ import java.util.Collections;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin(origins = "http://collectorui.herokuapp.com/*", allowCredentials = "true")
+@CrossOrigin(origins = "*", allowCredentials = "true")
 //@CrossOrigin(allowCredentials = "true")
 public class CollectorController {
 
